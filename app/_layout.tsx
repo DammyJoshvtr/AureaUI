37e4c1e5cc7cf6f47bc52b70d6e823553e7b2385
Damilola Joshua <damilolajoshua2021@gmail.com>
2026-01-10 07:01:49 +0100

    added images

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,16 @@ import { Stack } from "expo-router";
 import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack>
+        <Stack.Screen
+          name="home"
+        />
+        <Stack.Screen
+          name="auth_signup"
+        />
+      </Stack>
+    </>
+  );
 }
---
+++ constant/icon.ts
@@ -0,0 +1,7 @@
+export const icons = {
+  apple: require("@/assets/icons/apple.png"),
+  check: require("@/assets/icons/check.png"),
+  facebook: require("@/assets/icons/facebook.png"),
+  google: require("@/assets/icons/google.png"),
+  xcircle: require("@/assets/icons/xcircle.png"),
+};
