9ae81caa735d3aaf881e8fdc69066f00ce0aca7c
Damilola Joshua <damilolajoshua2021@gmail.com>
2026-02-25 23:31:04 +0100

    Corrected Component Name

--- app/(tabs)/analyst.tsx
+++ app/(tabs)/analyst.tsx
@@ -1,140 +1,14 @@
-// import { useRef, useState } from "react";
-// import { View } from "react-native";
-// import { LocaleConfig, WeekCalendar } from "react-native-calendars";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-// LocaleConfig.locales["fr"] = {
-//   monthNames: [
-//     "Janvier",
-//     "Février",
-//     "Mars",
-//     "Avril",
-//     "Mai",
-//     "Juin",
-//     "Juillet",
-//     "Août",
-//     "Septembre",
-//     "Octobre",
-//     "Novembre",
-//     "Décembre",
-//   ],
-//   monthNamesShort: [
-//     "Janv.",
-//     "Févr.",
-//     "Mars",
-//     "Avril",
-//     "Mai",
-//     "Juin",
-//     "Juil.",
-//     "Août",
-//     "Sept.",
-//     "Oct.",
-//     "Nov.",
-//     "Déc.",
-//   ],
-//   dayNames: [
-//     "Dimanche",
-//     "Lundi",
-//     "Mardi",
-//     "Mercredi",
-//     "Jeudi",
-//     "Vendredi",
-//     "Samedi",
-//   ],
-//   dayNamesShort: ["Dim.", "Lun.", "Mar.", "Mer.", "Jeu.", "Ven.", "Sam."],
-//   today: "Aujourd'hui",
-// };
-
-// LocaleConfig.defaultLocale = "fr";
-
-// const Analyst = () => {
-//   const [selected, setSelected] = useState("");
-//   const marked = useRef(getMarkedDates());
-
-//   return (
-//     <View className="flex-1 bg-white pt-12">
-//       <WeekCalendar
-//         onDayPress={(day) => {
-//           setSelected(day.dateString);
-//         }}
-//         markedDates={marked.current}
-//         firstDay={1}
-//       />
-//     </View>
-//   );
-// };
-
-// export default Analyst;
-
-// // selected
-// //   ? {
-// //       [selected]: {
-// //         selected: true,
-// //         disableTouchEvent: true,
-// //       },
-// //     }
-// //   : {}
-
-// import { useState } from "react";
-// import { View } from "react-native";
-// import { WeekCalendar } from "react-native-calendars";
-
-// const Analyst = () => {
-//   const today = new Date().toISOString().split("T")[0];
-//   const [selected, setSelected] = useState(today);
-
-//   return (
-//     <View className="flex-1 bg-white pt-12">
-//       <WeekCalendar
-//         firstDay={1}
-//         current={today}
-//         onDayPress={(day) => setSelected(day.dateString)}
-//         markedDates={{
-//           [selected]: { selected: true },
-//         }}
-//         theme={
-//           {
-//             selectedDayBackgroundColor: "#4630EB",
-//             selectedDayTextColor: "#fff",
-
-//             "stylesheet.day.basic": {
-//               base: {
-//                 width: 36,
-//                 height: 48,
-//                 alignItems: "center",
-//                 justifyContent: "center",
-//               },
-//               selected: {
-//                 borderRadius: 8,
-//               },
-//             },
-//           } as any
-//         }
-//       />
-//     </View>
-//   );
-// };
-
-// export default Analyst;
-
-import WeekCalendar from "@/components/calendars";
-import React, { useState } from "react";
-import { StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-
-const Analyst: React.FC = () => {
-  const [date, setDate] = useState(new Date());
-
+const Analyst = () => {
   return (
-    <SafeAreaView style={styles.safe}>
-      <WeekCalendar date={date} onChange={(newDate) => setDate(newDate)} />
-    </SafeAreaView>
+    <View>
+      <Text>Analyst</Text>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  safe: {
-    flex: 1,
-  },
-});
-
 export default Analyst;
+
+const styles = StyleSheet.create({});
